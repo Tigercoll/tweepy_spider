222185069e382c7dd4d05945c977b8d4a6adc120
Tiger <425860075@qq.com>
2019-03-30 11:48:41 +0800

    1
    1

--- process_data.py
+++ process_data.py
@@ -45,37 +45,37 @@ data_df = pd.read_json(data_json_str)
 # print(data_df['user_name'])
 process_data = ProcessData()
 # 获取用户是否关注 A关注B 为 1  否则为0
-# process_data.get_follow(data_df)
+process_data.get_follow(data_df)
 
 # 获取关注信息绘制图
-# import matplotlib.pyplot as plt
-# import networkx as nx
-# def draw_picture(filename):
-#     # 打开保存的csv图片
-#     with open(filename, 'r') as f:
-#         # 定义两个列表,有关注的列表跟没有关注的列表
-#         follow_list = []
-#         unfollow_list = []
-#         for line in f:
-#             # 获取是否关注
-#             is_follow = line.strip().split(',')[2]
-#             # 判断关注
-#             if is_follow == '1':
-#                 follow_list.append(line.strip().split(','))
-#             else:
-#                 unfollow_list.append(line.strip().split(',')[0])
-#     # networkx绘制关系有向图的神器,下次再讲
-#     DG = nx.DiGraph()
-#     # 添加节点
-#     # 是一个列表
-#     DG.add_nodes_from(unfollow_list)
-#     # 添加有向节点 为一个两元,或三元列表(A,B,int)表示为A到B
-#     DG.add_weighted_edges_from(follow_list)
-#     # 画图
-#     nx.draw(DG, with_labels=True)
-#     # 展示
-#     plt.show()
-# filename = 'following.csv'
-# draw_picture(filename)
+import matplotlib.pyplot as plt
+import networkx as nx
+def draw_picture(filename):
+    # 打开保存的csv图片
+    with open(filename, 'r') as f:
+        # 定义两个列表,有关注的列表跟没有关注的列表
+        follow_list = []
+        unfollow_list = []
+        for line in f:
+            # 获取是否关注
+            is_follow = line.strip().split(',')[2]
+            # 判断关注
+            if is_follow == '1':
+                follow_list.append(line.strip().split(','))
+            else:
+                unfollow_list.append(line.strip().split(',')[0])
+    # networkx绘制关系有向图的神器,下次再讲
+    DG = nx.DiGraph()
+    # 添加节点
+    # 是一个列表
+    DG.add_nodes_from(unfollow_list)
+    # 添加有向节点 为一个两元,或三元列表(A,B,int)表示为A到B
+    DG.add_weighted_edges_from(follow_list)
+    # 画图
+    nx.draw(DG, with_labels=True)
+    # 展示
+    plt.show()
+filename = 'following.csv'
+draw_picture(filename)
 
 
